d7f6dbb387f40295841b32fe8b0e10624d3fd83d
Richard Boardman <rickschmoo@gmail.com>
2018-06-19 21:47:59 +0100

    End of Chapter 25

--- src/index.js
+++ src/index.js
@@ -1,42 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const YOUTUBE_API_KEY='AIzaSyDX-9_zJuMQmVS4vLk-hF902XI43gHFCeo';
 
-// Create new component to produce HTML
-// const App = function() {
-// 	return <div>
-// 		Hi!
-// 		<ol>
-// 			<li>1</li>
-// 			<li>2</li>			
-// 			<li>3</li>
-// 		</ol>
-// 	</div>;
-// }
+class App extends Component {
 
-YTSearch(
-	{key: YOUTUBE_API_KEY, term: 'airfields'}, 
-	function(data) {
-		console.log(data);
+	constructor(props) {
+		super(props);
+		this.state = {
+			videos: []
+		}
+		YTSearch(
+			{key: YOUTUBE_API_KEY, term: 'airfields'}, 
+			(videos) => {
+				console.log(videos);
+				this.setState({
+					videos
+				});
+			}
+		);
 	}
-);
 
-const App = () => {
-	return (
-		<div>
-			Hi!
-			<ol>
-				<li>1</li>
-				<li>2</li>			
-				<li>3</li>
-			</ol>
-			<SearchBar />
-		</div>
-	);	
+	render () {
+		return(
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);	
+	}
 }
 
 // Place component's generated HTML in DOM
