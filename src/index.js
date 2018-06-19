71043f1759be9c5db4c170667f7f8d7b729dc74d
Richard Boardman <rickschmoo@gmail.com>
2018-06-19 16:15:24 +0100

    Basic componnt import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const YOUTUBE_API_KEY='AIzaSyDX-9_zJuMQmVS4vLk-hF902XI43gHFCeo';
 
 // Create new component to produce HTML
 // const App = function() {
@@ -14,14 +17,17 @@ import ReactDOM from 'react-dom';
 // }
 
 const App = () => {
-	return <div>
-		Hi!
-		<ol>
-			<li>1</li>
-			<li>2</li>			
-			<li>3</li>
-		</ol>
-	</div>;
+	return (
+		<div>
+			Hi!
+			<ol>
+				<li>1</li>
+				<li>2</li>			
+				<li>3</li>
+			</ol>
+			<SearchBar />
+		</div>
+	);	
 }
 
 // Place component's generated HTML in DOM
