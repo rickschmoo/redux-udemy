faf735ba0befc6201e5d64af51366a8f86dc24ef
Richard Boardman <rickschmoo@gmail.com>
2018-06-19 21:57:55 +0100

    Video thumbnails

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,28 @@
  import React from 'react';
 
- const VideoListItem = (props) => {
+ const VideoListItem = ({video}) => {
+
+ 	// above argument is equivalent to 'const video=props.video;''
+ 	console.log(video);
+
+ 	const imageUrl = video.snippet.thumbnails.default.url;
+
  	return (
- 		<li>{props.etag}</li>
+ 		<li className="list-group-item">
+ 			<div className="video-list media">
+ 				<div className="media-left">
+ 					<img
+ 						className="media-object"
+ 						src={imageUrl}
+ 					/>
+ 				</div>
+ 				<div className="media_body">
+ 					<div className="media-heading">
+ 						{video.snippet.title}
+ 					</div>
+ 				</div>
+ 			</div>
+ 		</li>
  	);
  };
 
